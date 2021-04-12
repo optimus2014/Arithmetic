65a2e7fc6f1fd897f5882e9190de47561ea8968c
optimus2014 <forever.yjr@163.com>
2021-04-12 19:26:43 +0800

    布隆过滤器，还差： 1.Hash函数的选择 2.容器大小，种子数k的大小，与误差率之间的关系 没有搞定。

--- src/main/java/com/Algorithm/Classical/BloomFilter.java
+++ src/main/java/com/Algorithm/Classical/BloomFilter.java
@@ -1,7 +1,9 @@
 package com.Algorithm.Classical;
 
-import sun.security.util.Length;
 
+import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
+
+import java.util.BitSet;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -32,17 +34,63 @@ import java.util.Set;
 public class BloomFilter {
     public static void main(String[] args) {
         System.out.println("这是布隆过滤器");
-        Set source = new HashSet();
-        source.add("a");
-        source.add("b");
-        BloomFilter bf = new BloomFilter(10,source,3);
-        System.out.println(bf.b.length);
+        Set<String> source = loadDataSet(data);
+        BloomFilter bf = new BloomFilter(source.size(),3);
+        for (String item : source){
+            bf.add(item);
+        }
+        System.out.println("K：" + bf.seedLen);
+        System.out.println("存在：" + bf.checkItem("哈"));
+        System.out.println("存在：" + bf.checkItem("这是Hash"));
+
+        System.out.println("原始数据大小：" + ObjectSizeCalculator.getObjectSize(data));
+        System.out.println("转化之后的Set大小：" + ObjectSizeCalculator.getObjectSize(source));
+        System.out.println("BloomFilter大小：" + ObjectSizeCalculator.getObjectSize(bf.bloom));
+        System.out.println("BloomFilter：" + bf.bloom);
+        bf.add("眼");
+        System.out.println("存在：" + bf.checkItem("眼"));
 
     }
 
+    /**
+     * 加载数据，转化为Set
+     * @param data1
+     * @return
+     */
+    public static Set<String> loadDataSet(String data1){
+        Set<String> source = new HashSet<String>();
+        for(int i = 0 ; i < data1.length(); i ++){
+            if(data1.charAt(i) == '\n' || data1.charAt(i) == '\0' || data1.charAt(i) == ' '){
+                continue;
+            }
+            source.add(String.valueOf(data1.charAt(i)));
+        }
+        System.out.println("转化之后的Set长度：" + source.size());
+        return source;
+    }
+
+    /****************** 以下是BloomFilter代码 *********************/
+
+    private static final int DEFAULT_SIZE = 2 << 10;
+    private static final int[] seeds = {2,4,12,54,7,3,27,32,1,45,25,98,43,6};
+    private static final HashOperator[] func = new HashOperator[seeds.length];
 
     // 二进制容器
-    private boolean[] b;
+    private BitSet bloom;
+    private int seedLen = seeds.length;
+
+    // 静态方法，JVM加载类时进行初始化
+//    static {
+//        for(int i = 0; i < seeds.length; i ++){
+//            func[i] = new HashOperator(DEFAULT_SIZE,seeds[i]);
+//        }
+//    }
+    private void init(int cap){
+        this.bloom = new BitSet(cap);
+        for(int i = 0; i < seeds.length; i ++){
+            func[i] = new HashOperator(cap,seeds[i]);
+        }
+    }
 
     /**
      * 参数释义：
@@ -50,31 +98,68 @@ public class BloomFilter {
      * sourceSet：原始待检验的Set集合
      * k：Hash函数个数。
      * */
-    public BloomFilter(int n, Set sourceSet, int k){
-        this.b = new boolean[n];
-        this.b[n/2] = true;
-        for (boolean i : this.b) {
-//            i = false;
-            System.out.println(i);
-
-        }   // 这一句可能就不需要，默认全部是False
-
-
+    public BloomFilter(){
+        init(DEFAULT_SIZE);
     }
-
+    public BloomFilter(int n, int k){
+        // 获取seed种子长度
+        if(k > seeds.length || k <= 0){
+            // k 超出现有种子库长度时，模式使用最长种子库
+            this.seedLen = seeds.length;
+        } else {
+            this.seedLen = k;
+        }
+        // 初始化BF容器大小
+        this.bloom = new BitSet(n);
+        init(n);
+    }
 
 
     // 检验函数，判断Item是否在原始集合中
     public boolean checkItem(String item){
+        if(item == null){
+            return false;
+        }
+        for(int i = 0; i < seedLen; i ++){
+            if(!bloom.get(func[i].hash(item))){
+                return false;
+            }
+        }
         return true;
     }
     // 添加元素
-    public void addItem(String item){
-        // this.b
+    public void add(String item){
+        for(int i = 0; i < seedLen; i ++){
+            bloom.set(func[i].hash(item));
+        }
     }
 
-    // Hash 函数内部类
-//    https://www.cnblogs.com/xiaobaituyun/p/11011393.html
+    /*******************
+     * Hash函数相关内容
+     * TODO:Hash函数做的还有问题，所有seed得到的最终结果都一样
+     */
+    // Hash 函数内部类(函数可以替换，MD5就是一个经典的Hash函数)
+    public static class HashOperator {
+        private int cap;   // ? cap是分桶数？
+        private int seed;  // 不同Hash函数的种子不同
+        public HashOperator(int cap,int seed){
+            this.cap = cap;
+            this.seed = seed;
+        }
+
+        public int hash(String value){
+            // Hash函数方法待定
+            int result = 0;
+            int len = value.length();
+            for(int i = 0;i < len; i ++){
+                // 注：之前遇到的一个坑，字符串长度为1时，seed失效，for循环只会遍历一个数值
+                result = (result << this.seed)  + value.charAt(i) >> 3;
+            }
+            return  (this.cap - 1) & result;  //
+        }
+    }
+
+    static String data= "值得注意的是，4 这个 bit 位由于两个值的哈希函数都返回了这个 bit 位，因此它被覆盖了。现在我们如果想查询 “dianping” 这个值是否存在，哈希函数返回了 1、5、8三个值，结果我们发现 5 这个 bit 位上的值为 0，说明没有任何一个值映射到这个 bit 位上，因此我们可以很确定地说 “dianping” 这个值不存在。而当我们需要查询 “baidu” 这个值是否存在的话，那么哈希函数必然会返回 1、4、7，然后我们检查发现这三个 bit 位上的值均为 1，那么我们可以说 “baidu” 存在了么？答案是不可以，只能是 “baidu” 这个值可能存在。这是为什么呢？答案跟简单，因为随着增加的值越来越多，被置为 1 的 bit 位也会越来越多，这样某个值 “taobao” 即使没有被存储过，但是万一哈希函数返回的三个 bit 位都被其他值置位了 1 ，那么程序还是会判断 “taobao” 这个值存在。";
 }
 
 

f09aa47bcd32f3d0fabcfac00d9a1d4fa47cca38
Yanru <forever.yjr@163.com>
2021-10-11 23:48:16 +0800

    添加LRU注释

--- src/main/java/com/Structure/Link/LeetCode/LRU.java
+++ src/main/java/com/Structure/Link/LeetCode/LRU.java
@@ -4,7 +4,13 @@ import com.Structure.Link.Learning.LinkNode;
 
 /**
  * 使用链表实现LRU算法(缓存淘汰算法)
- * LRU：最近最少使用的缓存机制
+ *
+ * LRU：最近最少使用算法
+ * 是一种缓存机制，常见的缓存机制有FIFO(先进先出)，LFU(最近最少使用)，LRU(最近最久未使用)
+ * 缓存算法是针对CPU缓存已满时，判断应该清除哪部分存储内容。
+ *
+ * LRU算法内容
+ *
  */
 public class LRU {
 

325deab8e84ba9b1453731289c0aaa8ebdc10f4c
Yanru <forever.yjr@163.com>
2021-10-17 00:21:54 +0800

    完成堆排序代码

--- src/main/java/com/Algorithm/Sort/HeapSort.java
+++ src/main/java/com/Algorithm/Sort/HeapSort.java
@@ -1,5 +1,7 @@
 package com.Algorithm.Sort;
 
+import java.util.Random;
+
 /***
  * 堆排序
  * 使用二叉树模式
@@ -15,18 +17,44 @@ package com.Algorithm.Sort;
  */
 public class HeapSort {
     public static void main(String[] args) {
+        int[] data = new int[8];
+        for (int i = 0; i < 8; i++){
+            data[i] = new Random().nextInt(20) + 1;
+        }
+
+        HeapSort.heapSord(data);
 
 
     }
 
-    public int[] heapSord(int[] data){
+    public static int[] heapSord(int[] data){
+        show(data);
         int len = data.length;
-        // 初始化数组
 
-        // 构建大根堆
+        /**
+         * 步骤一：构建大根堆
+         * */
+        // 最后一个非叶子结点
+        int end = len / 2 - 1;
 
-        // 不断置换元素，不断调整堆，直到堆中只保留一个元素。
+        for (int i = end ; i >= 0 ; i--){
+            heapAdjust(data,len,i);
+        }
+        show(data);
+        /**
+         * 步骤二：置换最大值，调整大根堆
+         * */
+        for(int i = data.length - 1; i > 0 ; i --){
+            // 第一个元素(最大值)置换为最末尾值
+            int tmp_value = data[0];
+            data[0] = data[i];
+            data[i] = tmp_value;
+            // 待比对数组长度减小1(最末尾的元素已经确定)
+            len --;
+            heapAdjust(data,len,0);
 
+        }
+        show(data);
         return data;
     }
 
@@ -36,7 +64,42 @@ public class HeapSort {
      * int len：待调整的数组总长度
      * int index：待调整元素下标
      * */
-    private void heapAdjust(int[] data,int len,int index){
-//        if ()
+    private static void heapAdjust(int[] data,int len,int index){
+        /**
+         * 递归函数结束条件:
+         * 叶子结点，
+         * 当前结点最大
+         * */
+        int tmp_index = max(data,len,index);
+        if (tmp_index == index){
+            return;
+        }
+
+        // 交换最大值
+        int tmp_value = data[index];
+        data[index] = data[tmp_index];
+        data[tmp_index] = tmp_value;
+        heapAdjust(data,len,tmp_index);
+    }
+    // 获取当前index结点，和左右结点中，最大元素下标
+    private static int max(int[] data,int len,int index){
+        int max = index;
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+        if (left < len && data[left] > data[max]){
+            max = left;
+        }
+
+        if (right < len && data[right] > data[max]){
+            max = right;
+        }
+        return max;
+    }
+    private static void show(int[] data){
+        System.out.print("当前数组：");
+        for(int i = 0; i < data.length; i ++){
+            System.out.printf("  %s;", data[i]);
+        }
+        System.out.println();
     }
 }

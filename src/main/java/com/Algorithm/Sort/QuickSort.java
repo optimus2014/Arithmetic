f8aba014ea078aaaf63cea470284f6547185f300
optimus2014 <forever.yjr@163.com>
2021-10-18 18:08:17 +0800

    今天更新了个寂寞

---
+++ src/main/java/com/Algorithm/Sort/QuickSort.java
@@ -0,0 +1,15 @@
+package com.Algorithm.Sort;
+
+/**
+ * 快速排序
+ * 算法思想：分治法（分而治之）
+ */
+public class QuickSort implements SortTool{
+    public static void main(String[] args) {
+        System.out.println("快速排序：");
+    }
+    @Override
+    public int[] sort(int[] data) {
+        return new int[0];
+    }
+}
---
+++ src/main/java/com/Algorithm/Sort/SortTool.java
@@ -0,0 +1,10 @@
+package com.Algorithm.Sort;
+
+/**
+ * 定义排序接口
+ */
+public interface SortTool {
+    // 排序方法
+    public int[] sort(int[] data);
+
+}

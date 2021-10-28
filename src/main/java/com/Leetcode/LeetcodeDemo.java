d63d24fbee2a66206c00f6f5f4ee04269c0d6db3
optimus2014 <forever.yjr@163.com>
2021-10-28 19:04:33 +0800

    Leetcode

--- src/main/java/com/Leetcode/LeetcodeDemo.java
+++ src/main/java/com/Leetcode/LeetcodeDemo.java
@@ -1,10 +1,23 @@
 package com.Leetcode;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class LeetcodeDemo {
     public static void main(String[] args) {
+
         LeetcodeDemo.show(LeetcodeDemo.twoSum(new int[]{3,2,4},6));
+        LeetcodeDemo.show(LeetcodeDemo.generateParenthesis(2));
+        LeetcodeDemo.show(LeetcodeDemo.generateParenthesis(3));
+        LeetcodeDemo.show(LeetcodeDemo.generateParenthesis(4));
+
+        /**
+         * "(())","()()"
+         * ["((()))","(()())","(())()","()(())","()()()"]
+         * ["(((())))","((()()))","((())())","((()))()","(()(()))","(()()())","(()())()","(())(())","(())()()","()((()))","()(()())","()(())()","()()(())","()()()()"]
+         */
+
     }
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
@@ -24,5 +37,37 @@ public class LeetcodeDemo {
         for(int d : data){
             System.out.print(d + "-> ");
         }
+        System.out.println();
+    }
+    private static void show(List data){
+        for(Object d : data){
+            System.out.print(d + "-> ");
+        }
+        System.out.println();
+    }
+
+
+    public static List<String> generateParenthesis(int n) {
+        // 归纳法
+        LinkedList<String> result = new LinkedList<String>();
+        result.add("()");
+        for (int i = 2;i <= n ; i++){
+            int len = result.size();
+            for(int j = 0; j < len; j ++){
+                result.add("(" + result.get(j) + ")");
+                if(!(result.get(j) + "()").equals("()" + result.get(j))){
+                    result.add("()" + result.get(j));
+                    result.add(result.get(j) + "()");
+                } else {
+                    result.add(result.get(j) + "()");
+                }
+
+            }
+            // 删除无效
+            for(int j = 0; j < len; j ++){
+                result.remove(0);
+            }
+        }
+        return result;
     }
 }
